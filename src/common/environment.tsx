70f9284b4552ddc782bcbcd16729ec728dec4b75
Shannon D <sdillman@users.noreply.github.com>
2021-09-26 01:34:45 -0400

    First stab at gathering some type definitions / interfaces in one place. Also adding the handy environment file from BlueBadge for ease of deployment

---
+++ src/common/environment.tsx
@@ -0,0 +1,14 @@
+let APIURL: string = '';
+
+switch (window.location.hostname) {
+    //this is the local host name for react app
+    case 'localhost' || '127.0.0.1':
+        //this is the local host name of your API
+        APIURL = 'http://localhost:3000';
+        break;
+    //this is the depoloyed react app - model from blue badge project
+    // case 'listo-sre.herokuapp.com' :
+    // APIURL = 'https://listo-efa.herokuapp.com'
+}
+
+export default APIURL;
---
+++ src/common/index.d.ts
@@ -0,0 +1,20 @@
+interface CocktailObj {
+    cocktailID?: number;
+    cocktailName: string;
+    imgURL?: string;
+    instructions?: JSON;
+    userID: number;
+    getComments: (userID?: number) => {};
+}
+
+interface CommentObj {
+    commentID?: number;
+    cocktailID: number;
+    content?: string;
+    userID: number;
+}
+
+export {
+    CocktailObj,
+    CommentObj
+};
