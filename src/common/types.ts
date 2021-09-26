ada9bc0505ae664676ded40efc12abdb0b90a05d
Shannon D <sdillman@users.noreply.github.com>
2021-09-26 03:15:28 -0400

    Adding a cocktails get file, still needs work. Also additions to types.ts

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,8 +1,8 @@
 interface CocktailObj {
-    cocktailID?: number;
-    cocktailName: string;
-    imgURL?: string;
-    instructions?: JSON;
+    cocktailID?: number;  // from local DB if the cocktail has been submitted. Will not be present yet if it has not
+    cocktailName: string;  // "strDrink":"The Drink Name" from CocktailDB
+    imgURL?: string;  // "strDrinkThumb" from CocktailDB
+    instructions?: JSON;  // need to create an object from "strInstructions" plus whichever of "strIngredient1"(thru 15) and "strMeasure1" (thru 15) have content. Need to match 1 - 2, 2 - 2, etc
     userID: number;
     getComments: (userID?: number) => {};
 }
@@ -14,7 +14,67 @@ interface CommentObj {
     userID: number;
 }
 
+interface cocktailDBResponse {
+    drinks?: (DrinksEntity)[] | null;
+}
+
+interface DrinksEntity {
+    idDrink: string;
+    strDrink: string;
+    strDrinkAlternate?: string | null;
+    strTags?: string | null;
+    strVideo?: string | null;
+    strCategory: string;
+    strIBA?: string | null;
+    strAlcoholic: string;
+    strGlass: string;
+    strInstructions: string;
+    strInstructionsES?: string | null;
+    strInstructionsDE: string;
+    strInstructionsFR?: string | null;
+    strInstructionsIT: string;
+    "strInstructionsZH-HANS"?: string | null;
+    "strInstructionsZH-HANT"?: string | null;
+    strDrinkThumb: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4?: string | null;
+    strIngredient5?: string | null;
+    strIngredient6?: string | null;
+    strIngredient7?: string | null;
+    strIngredient8?: string | null;
+    strIngredient9?: string | null;
+    strIngredient10?: string | null;
+    strIngredient11?: string | null;
+    strIngredient12?: string | null;
+    strIngredient13?: string | null;
+    strIngredient14?: string | null;
+    strIngredient15?: string | null;
+    strMeasure1: string;
+    strMeasure2?: string | null;
+    strMeasure3?: string | null;
+    strMeasure4?: string | null;
+    strMeasure5?: string | null;
+    strMeasure6?: string | null;
+    strMeasure7?: string | null;
+    strMeasure8?: string | null;
+    strMeasure9?: string | null;
+    strMeasure10?: string | null;
+    strMeasure11?: string | null;
+    strMeasure12?: string | null;
+    strMeasure13?: string | null;
+    strMeasure14?: string | null;
+    strMeasure15?: string | null;
+    strImageSource?: string | null;
+    strImageAttribution?: string | null;
+    strCreativeCommonsConfirmed: string;
+    dateModified: string;
+}
+  
 export type {
+    cocktailDBResponse,
     CocktailObj,
-    CommentObj
+    CommentObj,
+    DrinksEntity
 };
---
+++ src/components/cocktails/CocktailsGet.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import dotenv from 'dotenv';
+import { CocktailObj } from "../../common/types";
+
+dotenv.config();
+
+interface State {
+    cocktails: CocktailObj[];
+    token: string;
+}
+
+interface Props {
+    editCocktailsList?: () => void;
+    deleteCocktailsList?: () => void;
+    token: string;
+    updateOn: (newToken: string) => void;
+}
+
+let cocktailRandomURL = `https://www.thecocktaildb.com/api/json/v2/${process.env.APIKEY}/random.php`;
+
+console.log(cocktailRandomURL);
+
+/**
+ * Renders a table with 1 row for each cocktail.
+ */
+class CocktailsGet extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            token: this.props.token,
+            cocktails: []
+        };
+    }
+
+    componentDidMount() {
+        this.fetchCocktailList();
+    }
+
+    /**
+     * Fetches a list of cocktails based on the user's token.
+     */
+    async fetchCocktailList() {
+        const cocktails: CocktailObj[] = await fetch('http://localhost:3000/cocktail/mine', {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.state.token}`
+            })
+        }).then( data => data.json())
+        .catch(console.error);
+        if (cocktails.length) {
+            this.setState({ cocktails });
+        }
+    }
+
+    /**
+     * Map over a list of cocktails and render them to a table row.
+     */
+    cocktailListMapper() {
+        return this.state.cocktails.map((item: CocktailObj) => {
+            return(
+                <tr key={item?.cocktailID}>
+                    <th scope="row">{item?.cocktailID}</th>
+                    <td>{item.cocktailName}</td>
+                    <td>{item?.imgURL ? <img src={item?.imgURL} /> : null}</td>
+                    <td>{item?.instructions}</td>
+                    <td>
+                        <Button color="info" onClick={() => {this.props?.editCocktailsList(); this.props.updateOn()}}>Update</Button>
+                        <Button outline color="danger" onClick={() => {this.props?.deleteCocktailsList()}}>Delete</Button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>My Cocktails Collection</h1>
+                <table>
+                    {this.cocktailListMapper()}
+                </table>
+            </div>
+        );
+    };
+};
+
+export default CocktailsGet;
